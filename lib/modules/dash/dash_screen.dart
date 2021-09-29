2c96dbe6a4d67a093907fcc4a30d7e46139002eb
Hugo Streppel Silva <strh93@gmail.com>
2021-09-28 23:10:40 -0300

    componente de cartao da tela de dash

--- lib/modules/dash/dash_screen.dart
+++ lib/modules/dash/dash_screen.dart
@@ -8,7 +8,85 @@ class DashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
-      body: Center(),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        DashCard(),
+                        DashCard(),
+                        DashCard(),
+                        DashCard(),
+                        DashCard(),
+                        DashCard(),
+                        DashCard(),
+                        DashCard(),
+                      ]),
+                  Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        DashCard(),
+                        DashCard(),
+                        DashCard(),
+                        DashCard(),
+                        DashCard(),
+                        DashCard(),
+                        DashCard(),
+                        DashCard(),
+                      ])
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DashCard extends StatelessWidget {
+  const DashCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 200,
+      height: 150,
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Item'),
+              subtitle: Text('Descrição'),
+            ),
+            ButtonBar(
+              children: [
+                TextButton(
+                  child: const Text('Excluir'),
+                  onPressed: () {
+                    // TODO
+                  },
+                ),
+                TextButton(
+                  child: const Text('Editar'),
+                  onPressed: () {
+                    // TODO
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
